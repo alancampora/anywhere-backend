16aa9896df3b3eddf1019cd6e0cfc2897baee342
Alan Campora <alan.campora@sovanta.com>
2017-08-13 16:44:15 +0200

    [ADD] blabla: formatting data

--- server.js
+++ server.js
@@ -34,10 +34,34 @@ app.get('/get/', (req, res) => {
 
 app.get('/get/blabla', (req, res) => {
     const query = req.query,
-        targetURL = nearbyCitiesService + `get/?lat=${query.lat}&lon=${query.lon}&radius=${query.radius}`;
-    rp(targetURL)
+        baseUrl = "https://public-api.blablacar.com/api/v2/trips?",
+        key = "key=bec1eafb73de4b77a3934f0b7088d674",
+        fc = "fc=",
+        page = "page=",
+        url = baseUrl + fc + encodeURI(query.lat + '|' + query.lon) + "&" + key + "&" + page + query.page;
+
+    console.log("blabla", url);
+    rp(url)
         .then(function(data) {
-            return res.json(JSON.parse(data));
+            const results = [];
+            data = JSON.parse(data);
+
+            data.trips.forEach(trip => {
+                let splittedDate = trip.departure_date.split(" "),
+                    formattedData = {
+                        arrivalPlace: {},
+
+                    };
+                formattedData.arrivalPlace.latitude = trip.arrival_place.latitude;
+                formattedData.arrivalPlace.longitude = trip.arrival_place.longitude;
+                formattedData.arrivalPlace.cityName = trip.arrival_place.city_name;
+                formattedData.departureDate = splittedDate[0];
+                formattedData.departureHour = splittedDate[1];
+                formattedData.duration = Math.round(trip.duration.value / 3600);
+                results.push(formattedData);
+            })
+            console.log(results);
+            return res.json(results);
         })
         .catch(function(err) {
             return res.json({
